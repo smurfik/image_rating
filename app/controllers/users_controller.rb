c4e2b703bf1b0207d19cc234613da447c6c7160f
smurfik <smurfik@mlpinit.com>
2016-01-05 15:58:44 -0800

    add sign-out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,19 @@ class UsersController < ApplicationController
   def sign_up
   end
 
+  def create
+    @user = User.new(params.require(:user).permit(:username, :email, :password, :password_confirmation))
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "Welcome!"
+    else
+      render :sign_up
+    end
+  end
+
+  def sign_out
+    session.delete(:user_id)
+    redirect_to root_path, notice: "See you soon!"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   root to: 'photos#main'
 
   get '/sign-up', to: 'users#sign_up', as: :sign_up
+  post '/sign-up', to: 'users#create'
+  get '/sign-out', to: 'users#sign_out', as: :sign_out
 
 end
