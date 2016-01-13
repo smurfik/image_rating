ea84abb91cab3dcb3cff1146e1790faa824e7ed3
smurfik <smurfik@mlpinit.com>
2016-01-13 11:45:20 -0800

    remove avatar from users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160110201820) do
+ActiveRecord::Schema.define(version: 20160113193528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,6 @@ ActiveRecord::Schema.define(version: 20160110201820) do
     t.string   "username"
     t.string   "email"
     t.string   "password_digest"
-    t.string   "avatar"
     t.boolean  "notification",    default: false
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
