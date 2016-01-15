27a5041e598c3abd3f868178eade85a584e2953a
smurfik <smurfik@mlpinit.com>
2016-01-15 14:18:51 -0800

    add validation for review

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,14 @@ class Review < ActiveRecord::Base
 
   validates :rating, presence: true
   validates :user_id, uniqueness: { scope: :photo_id }
+  validate :cannot_review_your_own_photos
   belongs_to :photo, counter_cache: true
   belongs_to :user
+
+  def cannot_review_your_own_photos
+    if photo.user_id == user_id
+      errors.add(:review, "can't review your own photo")
+    end
+  end
 
 end
