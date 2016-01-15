045e3030bee16fd9bdddf3176e6edd3219871f84
smurfik <smurfik@mlpinit.com>
2016-01-14 18:51:02 -0800

    add validation for unique user_id & photo_id for a review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ActiveRecord::Base
 
   validates :rating, presence: true
+  validates :user_id, uniqueness: { scope: :photo_id }
   belongs_to :photo, counter_cache: true
   belongs_to :user
 
